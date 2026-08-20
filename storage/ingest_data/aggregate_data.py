c1a6a96de26b1f07da8bfe2256dee74d2731cd62
ana-e-uk <uribe055@umn.edu>
2026-08-19 21:04:54 -0500

    describe script

--- storage/ingest_data/aggregate_data.py
+++ storage/ingest_data/aggregate_data.py
@@ -1,3 +1,13 @@
-from polaris.config import get_settings
+"""Aggregate the data blocks to coarser spatial and temporal resolutions.
 
-# For each bucket directory
+For every bucket that has one or more (new) blocks,
+    Coarsen blocks to the following temporal resolutions: {Hour, Day, Month, Year}
+    Store these coarser blocks
+    Coarsen blocks spatially by a factor of 2 and a factor of 4
+    Store these coarser blocks
+
+For every new temporally coarser block
+    Coarsen blocks spatially by a factor of 2 and a factor of 4
+
+"""
+from polaris.config import get_settings
